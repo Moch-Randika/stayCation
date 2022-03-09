f232703f06bb0f145882f36cbe4d0525564edc24
Mochammad Randika Hasanudin <limitesrandy@gmail.com>
2022-03-09 17:46:55 +0700

    men-validate dengan validator dan category crud done

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,8 +1,9 @@
 // controllers Category
 
+
 // import 
-const { request, response } = require("../app");
-const Category = require("../models/Category")
+const Category = require("../models/Category");
+const validate = require('validator');
 
 
 
@@ -16,7 +17,7 @@ const Category = require("../models/Category")
 
                 // alert 
                 const alertMessage = request.flash("alertMessage");
-                const alertStatus = request.flash("alertStatus")
+                const alertStatus = request.flash("alertStatus");
                 const alert = { message: alertMessage , status: alertStatus}
 
                 
@@ -25,59 +26,94 @@ const Category = require("../models/Category")
 
 
             } catch (error) {
-                response.redirect("admin/category")
+                response.redirect("admin/category", { alert : { message: `${error.message}` , status: "danger"} } )
             }
         
     }
 
     exports.createCategory = async (request,response) => {
 
-        try{
+            try{
 
-            const name = request.body.name;     
-            // create data .. 
-               const alreadyCategory =  await Category.exists({ name : name }) 
-               if (alreadyCategory ==  null ) {
+               const name = validate.trim(request.body.name);     
 
+            
+               const existsCategory =  await Category.exists({ name })
 
+               if (existsCategory ==  null ) {
+
+                    // create data .. 
                 await Category.create({ name });
                 // alert
-            request.flash("alertMessage", "Add category has been successfully")
-            request.flash("alertStatus", "success")
-            response.redirect("/admin/category");
+                request.flash("alertMessage", `Add category "${name}" has been successfully`)
+                request.flash("alertStatus", "success")
+                response.redirect("/admin/category");
 
                } else {
 
-               request.flash("alertMessage", "category is already exist")
-               request.flash("alertStatus", "warning")
+               request.flash("alertMessage", `category is "${name}" already exist`);
+               request.flash("alertStatus", "warning");
                response.redirect("/admin/category");
                }
 
             
-        }catch{
-            response.redirect("/admin/category");
-        }
+            }catch(error){
+                request.flash("alertMessage", `${error.message}`)
+                request.flash("alertStatus", "danger")
+                response.redirect("/admin/category");
+            }
        
-    }
+        }
 
     exports.updateCategory = async (request,response)=> {
 
-        const id = request.body.id;
-        const name = request.body.name;
+            try {
 
-        // find _id : key->id 
-        const category = await Category.findOne({_id : id})
-        category.name = name ; // overwrite key - name
-        await category.save();
-        response.redirect("/admin/category");
+
+                    const id = validate.trim(request.body.id);
+                    const name = validate.trim(request.body.name);
+                    const existsCategory  = await Category.exists({ name })
+        
+                if (existsCategory ==  null ){
+                    const categoryId = await Category.findOne({_id : id})
+                    categoryId.name = name ;  // find _id : key->id 
+                    // overwrite key - name
+                    await category.save();
+                    request.flash("alertMessage", `category "${name}" changed successfully`);
+                    request.flash("alertStatus", "success");
+                    response.redirect("/admin/category");
+                }else{
+                    request.flash("alertMessage", `category is "${name}" already exist`);
+                    request.flash("alertStatus", "warning");
+                    response.redirect("/admin/category");
+                }
+
+            } catch (error) {
+                    request.flash("alertMessage", `${error.message}`)
+                    request.flash("alertStatus", "danger")
+                    response.redirect("/admin/category");
+            }
+
+        
 
     }
 
     exports.deleteCategory = async (request,response) => {
 
-        const id = request.body.id;
-        await  Category.findOne({_id : id}).remove();
-        response.redirect("/admin/category");
+        try {
+            const id = validate.trim(request.body.id);
+            const name = validate.trim(request.body.name);
+            await  Category.findOne({_id : id}).remove();
+            request.flash("alertMessage", `category "${name}" deleted successfully`);
+            request.flash("alertStatus", "success");
+            response.redirect("/admin/category");
+        } catch (error) {
+            request.flash("alertMessage", `${error.message}`)
+            request.flash("alertStatus", "danger")
+            response.redirect("/admin/category");
+        }
+
+        
             
        
         
