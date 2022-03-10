eacab5f7bda87ecdca082bf302340e73048ab9ef
Mochammad Randika Hasanudin <limitesrandy@gmail.com>
2022-03-10 21:32:18 +0700

    category add to view item

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -1,11 +1,25 @@
 // controllers Dashboard
 
+// import
+const Category = require("../models/Category")
 
 
 // function 
-exports.indexItem = (request,response) => {
-
-    response.render("admin/item/index")
+exports.indexItem = async (request,response) => {
+    try {
+        const categories = await Category.find();
+        // alert 
+        const alertMessage = request.flash("alertMessage");
+        const alertStatus = request.flash("alertStatus");
+        const alert = { message: alertMessage , status: alertStatus}
+        response.render("admin/item/index",{ categories,alert})
+    } catch (error) {
+        request.flash("alertMessage", `${error.message}`)
+        request.flash("alertStatus", "danger")
+        // redirect
+        response.redirect("/admin/item");
+    }
+   
 }
 
 
